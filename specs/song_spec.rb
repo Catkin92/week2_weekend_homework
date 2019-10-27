972a316794542002fa621481f9a1fd79c17a4ac0
Catrin <catrinhughes92@gmail.com>
2019-10-27 20:34:06 +0000

    final commit

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -1,6 +1,7 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../song.rb')
+require_relative('../room.rb')
 
 class TestSong < MiniTest::Test
 
@@ -9,6 +10,13 @@ class TestSong < MiniTest::Test
     @song1 = Song.new("Barbie Girl", "Pop")
     @song2 = Song.new("Rock Me Amadeus", "Europop")
     @song3 = Song.new("I Will Always Love You", "Ballad")
+    @song4 = Song.new("Poker Face", "Pop")
+
+    @playlist = [@song1, @song2, @song3, @song4]
+
+    @guests = []
+
+    @room1 = Room.new(1, @playlist, @guests)
   end
 
   def test_get_name
@@ -18,4 +26,5 @@ class TestSong < MiniTest::Test
   def test_get_genre
     assert_equal("Ballad", @song3.genre)
   end
+
 end
